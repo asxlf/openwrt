ad1655653b438f8c34ca103eda92f243d4b8b4e0
asxlf <36530674+asxlf@users.noreply.github.com>
2023-05-03 15:05:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,3 +20,4 @@ echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.defau
 
 # Lienol/openwrt-package
 #sed -i '$a src-git lienol https://github.com/Lancenas/lienol-openwrt-package.git' feeds.conf.default
+#sed -i '$a src-git lienol https://github.com/xiaorouji/openwrt-package' feeds.conf.default
